308f4ae6940f77144bb30cd423192b6e7737d1c9
Shivom <shvomsrivastava@gmail.com>
2022-04-24 17:02:59 +0530

    🏗️ basic layout setup

--- src/App.js
+++ src/App.js
@@ -1,112 +1,67 @@
-import React from 'react';
-import logo from './images/logo.png';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useEffect, useState } from 'react';
 
-const App = () => (
-  <div className="flex min-h-screen flex-col justify-center bg-gray-100 py-6 sm:py-12">
-    <div className="relative py-3 sm:mx-auto sm:max-w-xl">
-      <div className="absolute inset-0 -skew-y-6 transform bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl" />
-      <div className="relative bg-white px-4 py-10 shadow-lg sm:rounded-3xl sm:p-20">
-        <div className="mx-auto max-w-md">
-          <div>
-            <a href="https://digitalinspiration.com/">
-              <img src={logo} className="h-7 sm:h-8" alt="Logo" />
-            </a>
-          </div>
-          <div className="divide-y divide-gray-200">
-            <div className="space-y-4 py-8 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7">
-              <p>
-                Quickly setup a React project with Create React App and Tailwind
-                CSS.
-              </p>
-              <ul className="list-disc space-y-2">
-                <li className="flex items-start">
-                  <span className="flex h-6 items-center sm:h-7">
-                    <svg
-                      className="h-5 w-5 flex-shrink-0 text-cyan-500"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                  <p className="ml-2">
-                    <code className="text-sm font-bold text-gray-900">
-                      npm run start
-                    </code>
-                    &nbsp;&mdash;&nbsp;Run the React app in development mode
-                    with live reloading.
-                  </p>
-                </li>
-                <li className="flex items-start">
-                  <span className="flex h-6 items-center sm:h-7">
-                    <svg
-                      className="h-5 w-5 flex-shrink-0 text-cyan-500"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                  <p className="ml-2">
-                    <code className="text-sm font-bold text-gray-900">
-                      npm run build
-                    </code>
-                    &nbsp;&mdash;&nbsp;Bundles the React app for deployment in
-                    production environment.
-                  </p>
-                </li>
-                <li className="flex items-start">
-                  <span className="flex h-6 items-center sm:h-7">
-                    <svg
-                      className="h-5 w-5 flex-shrink-0 text-cyan-500"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                  <p className="ml-2">
-                    <code className="text-sm font-bold text-gray-900">
-                      npm run inline
-                    </code>
-                    &nbsp;&mdash;&nbsp;Inline all CSS and JS in a single minfied
-                    file using Gulp.
-                  </p>
-                </li>
-              </ul>
-              <p>
-                All the unused CSS classes are automatically removed from the
-                production build with PurgeCSS.
-              </p>
-            </div>
-            <div className="pt-6 text-base font-bold leading-6 sm:text-lg sm:leading-7">
-              <p>
-                The React and Tailwind CSS starter kit is maintained by &nbsp;
-                <a
-                  href="https://digitalinspiration.com/"
-                  className="text-cyan-600 no-underline hover:text-cyan-700"
-                >
-                  Amit Agarwal &rarr;
-                </a>
-              </p>
-            </div>
+const handleRequest = async (movieIn, setMovies) => {
+  const res = await fetch(`localhost:5000/recommend/${movieIn}`);
+  const data = await res.json();
+  setMovies(data);
+};
+const App = () => {
+  const [movies, setMovies] = useState([1, 2, 3, 4, 5, 6]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  useEffect(() => {
+    handleRequest(selectedMovie, setMovies);
+  }, []);
+  return (
+    <div className="min-h-screen bg-slate-500 text-center text-white">
+      <div
+        className="mx-auto flex h-full w-full flex-col"
+        style={{ maxWidth: '1400px' }}
+      >
+        <header className="bg-slate-700 py-8 shadow-xl">
+          <h1 className="mb-4 block text-7xl font-bold uppercase">Spiell</h1>
+          <p className="text-3xl text-gray-200">
+            Content Based Movie Recommendation System
+          </p>
+        </header>
+        <div
+          className="flex-1 bg-slate-600 p-6 text-xl font-bold text-neutral-100"
+          style={{
+            background: 'url()',
+          }}
+        >
+          <label htmlFor="movie-name" className="mb-4 block">
+            Enter/Choose a movie name
+          </label>
+          <input
+            type="text"
+            id="movie-name"
+            name="movie-name"
+            placeholder="Avatar"
+            className="rounded-full text-black focus:shadow-lg"
+            onChange={(e) => setSelectedMovie(e.target.value)}
+            value={selectedMovie}
+          />
+          <div className="mt-8 flex justify-center gap-8">
+            {new Array(5).fill().map((i, idx) => (
+              <div
+                className="grid h-56 w-48 place-items-center overflow-hidden rounded-xl bg-blue-600 text-gray-300 shadow-2xl transition-all hover:scale-105"
+                key={idx}
+              >
+                This is a movie box <br /> {selectedMovie}
+              </div>
+            ))}
           </div>
         </div>
+        <footer className="bg-slate-800 p-8">
+          Made by Shivom Srivastava, TV Anish & PC Bhaskar
+          <span className="block">using React, Tailwind & Flask </span>
+        </footer>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
